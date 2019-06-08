08f5a428ef20d5fe1c173c5b78c5f4e9c11735d5
Alexandr Gorchakov <alexmakeresponsive@gmail.com>
2019-06-08 19:05:56 +0600

    test WP_Query

--- wp-content/themes/smarty/index.php
+++ wp-content/themes/smarty/index.php
@@ -1,5 +1,23 @@
 <?php
 
+$query = new WP_Query(array(
+	'posts_per_page'   => 10,
+));
+
+if ( $query->have_posts() ) {
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		$post_titles[]['title'] = get_the_title();
+	}
+} else {
+
+}
+wp_reset_postdata();
+
+// s($post_titles);
+
+
 $smarty->assign('name', 'Alex');
+$smarty->assign('PostTitles', $post_titles);
 
 $smarty->display('index.tpl');
--- wp-content/themes/smarty/templates/templates/index.tpl
+++ wp-content/themes/smarty/templates/templates/index.tpl
@@ -1,1 +1,7 @@
 Hello {$name}, welcome to Smarty!
+
+<ul>
+    {foreach $PostTitles as $item}
+        <li>{$item.title}</li>
+    {/foreach}    
+</ul>
